5d0dd8dbb51af3341ff62aa9ff7487eabaff2e1b
M1D0R1x <veerababusaviti21@gmail.com>
2025-06-25 11:07:17 +0530

    pulling check for aws liux

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
         <h1>hello veera</h1>
         <h2>this is my first app on AWS EC2</h2>
         <h3>deployed by veera</h3>
+        <h2>pulling check</h2>
     </div>
   )
 }
