f6a5c32e03c962a00e1302fca6897c3c00d1614e
M1D0R1x <veerababusaviti21@gmail.com>
2025-06-25 10:13:25 +0530

    Initial commit for the first ec2 instance of react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+        <h1>hello veera</h1>
+        <h2>this is my first app on AWS EC2</h2>
+        <h3>deployed by veera</h3>
+    </div>
+  )
+}
+
+export default App
