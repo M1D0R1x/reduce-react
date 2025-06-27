7eaf31da80d8c7ba2117870c9c625a9198aabe8f
M1D0R1x <veerababusaviti21@gmail.com>
2025-06-27 11:28:22 +0530

    push check aws linux

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import './App.css'
 function App() {
   return (
     <div>
-        <h1>hello harshitha</h1>
+        <h2>ws 2</h2>
+        <h1>hello veera</h1>
         <h2>this is my first app on AWS EC2</h2>
-        <h3>deployed by harshitha</h3>
+        <h3>deployed by veera</h3>
         <h2>pulling check</h2>
     </div>
   )
